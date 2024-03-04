ca756bac93cf2b17fefada4341570ec6905296a5
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-04 15:08:27 -0500

    Update login.php

--- login_page/login.php
+++ login_page/login.php
@@ -1,16 +1,18 @@
 //The code below connects the login page to the database to validate the credentials in order to log in
 <?php
 // Establish a connection to the database
-$servername = "tcp:usarcent2024.database.windows.net,1433";
-$Username = "USARCENT-HA";
-$Password = "TravisBobby2024!";
-$dbname = "USARCENTHousing-2024-2-21-19-19";
+$serverName = "tcp:usarcent2024.database.windows.net,1433";
+$connectionOptions = array(
+    "Database" => "USARCENTHousing-2024-2-21-19-19",
+    "Uid" => "USARCENT-HA",
+    "PWD" => "TravisBobby2024!",
+);
 
-$conn = new mysqli($servername, $Username, $Password, $dbname);
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 
 // Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    die(print_r(sqlsrv_errors(), true));
 }
 
 // Get user input from the form
@@ -18,15 +20,15 @@ $Username = $_POST['Username'];
 $Password = $_POST['Password'];
 
 // Protect against SQL injection
-$Username = mysqli_real_escape_string($conn, $Username);
-$Password = mysqli_real_escape_string($conn, $Password);
+$Username = sqlsrv_real_escape_string($conn, $Username);
+$Password = sqlsrv_real_escape_string($conn, $Password);
 
 // Query to check if the username and password match
 $sql = "SELECT * FROM users WHERE username='$Username' AND password='$Password'";
-$result = $conn->query($sql);
+$result = sqlsrv_query($conn, $sql);
 
 // Check if there is a match
-if ($result->num_rows > 0) {
+if (sqlsrv_has_rows($result)) {
     // Successful login
     header("Location: form.html");
     exit();
@@ -39,6 +41,6 @@ if ($result->num_rows > 0) {
 }
 
 // Close the database connection
-$conn->close();
+sqlsrv_close($conn);
 ?>
  
