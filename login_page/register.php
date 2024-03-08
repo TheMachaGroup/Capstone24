a451f14f04a36b030219fb0970d51bc0577c2f0f
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 10:19:42 -0500

    Update register.php

--- login_page/register.php
+++ login_page/register.php
@@ -1,28 +1,3 @@
-//this code has not been tested as of 3/7 
-
-<form id="account" action="#" method="POST">
-        <div id="container1">
-            <div id="container2">
-                <h1 id="title">Create account</h1>
-                <div class="prog-bar"></div>
-                <div id="step-1" class="steps">
-                    <label class="box_label">First Name</label>
-                    <input class="input_box" required>
-                    <label class="box_label">Last Name</label>
-                    <input class="input_box" required>
-                    <label class="box_label">Username</label>
-                    <input class="input_box" required>
-                    <label class="box_label">Password</label>
-                    <input class="input_box" required>
-                </div>
-                <button class="btn" id="cnt" type="button">Continue</button>
-            </div>
-            
-        </div>
-    </form>
-    <script type="text/javascript" src="step.js"></script>
-</body>
-</html>
 
 ?php if (isset($_POST['register'])) {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
