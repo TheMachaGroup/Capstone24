bbed600c1d505ba457303b6303ea0f90ab343beb
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 13:01:35 -0500

    Update register.php

--- login_page/register.php
+++ login_page/register.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
