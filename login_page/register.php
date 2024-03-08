004516d4fc7869318e19a36a55dc5397d9bffd56
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 12:19:54 -0500

    Update register.php

--- login_page/register.php
+++ login_page/register.php
@@ -1,36 +1,37 @@
-<?php
+?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-echo "connected to database"; 
-    
+    echo "Connected to database<br>";
+
     // Get the form data
     $FirstName = $_POST['FirstName'];
     $LastName = $_POST['LastName'];
     $Username = $_POST['Username'];
     $Password = $_POST['UserPassword'];
-    $Role = $_POST['Role']; 
-echo "received form data";
-    
+    $Role = $_POST['Role'];
+    echo "Received form data<br>";
+
     // Prepare and bind the SQL statement to check if the username already exists
-    $stmt = $conn->prepare('SELECT UserID, UserPassword FROM users WHERE username = $Username');
-    $stmt->bind_param('s', $username);
+    $stmt = $conn->prepare('SELECT UserID, UserPassword FROM users WHERE username = ?');
+    $stmt->bind_param('s', $Username);
     $stmt->execute();
     $stmt->store_result();
-    
+
     // Check if the account already exists in the database
     if ($stmt->num_rows > 0) {
         echo 'Account already exists, please create another!';
     } else {
         // Prepare and bind the SQL statement to insert a new account
-        $stmt = $conn->prepare('INSERT INTO users (FirstName, LastName, Username, UserPassword, Role) VALUES ($FirstName, $LastName, $Username, $Password, $Role)');
-    echo "entered into database";
-        
+        $stmt = $conn->prepare('INSERT INTO users (FirstName, LastName, Username, UserPassword, Role) VALUES (?, ?, ?, ?, ?)');
+        $stmt->bind_param('sssss', $FirstName, $LastName, $Username, $Password, $Role);
+        echo "Entered into database<br>";
+
         if ($stmt->execute()) {
-            Header("Location: https://usarcent.azurewebsites.net/form.html");
+            header("Location: https://usarcent.azurewebsites.net/form.html");
             exit();
         } else {
             echo 'Error creating account: ' . $stmt->error;
