4b381aba2c3682f77001e843255cb95646df2b69
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 09:29:53 -0500

    Update login.php

--- login_page/login.php
+++ login_page/login.php
@@ -1,33 +1,34 @@
-//The code below connects the login page to the database to validate the credentials in order to log in
 <?php
+function validate($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
 try {
     $conn = mysqli_init();
     mysqli_real_connect($conn, "usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 }
-catch (PDOException $e) {
+catch (Exception $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
 
 // Get user input from the form
-    $Username = validate($_POST['Username']);
-    $Password = validate($_POST['Password']);
+$Username = validate($_POST['Username']);
+$Password = validate($_POST['Password']);
 
-    if (empty($Username)) {
-        header("Location: index.php?error=Username is required");
-        exit();
-    }else if(empty($Password)){
-        header("Location: index.php?error=Password is required");
-        exit();
-    }else{$sql = "SELECT * FROM users WHERE Username='$Username' AND UserPassword='$Password'";
-          $result = mysqli_query($conn, $sql);
-          if (mysql_num_rows($result)) {
-              echo "hello";
-          }
+$sql = "SELECT * FROM users WHERE Username='$Username' AND UserPassword='$Password'";
+$result = mysqli_query($conn, $sql);
+
+if ($result) {
+    // Use mysqli_num_rows instead of mysql_num_rows
+    if (mysqli_num_rows($result) > 0) {
+        echo "hello";
+    } else {
+        echo "Invalid credentials";
     }
-}else{
-    header("Location: index.php");
-    exit();
+} else {
+    echo "Query failed: " . mysqli_error($conn);
+}
 
 // Close the database connection
 $conn->close();
