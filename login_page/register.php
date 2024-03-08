6bbdad8f45e7b9958ddd4ca73d45e900c9637165
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 10:11:49 -0500

    Update register.php

--- login_page/register.php
+++ login_page/register.php
@@ -1,14 +1,28 @@
 //this code has not been tested as of 3/7 
 
-<form action="register.php" method="post">
-  <label for="username">Username:</label> 
-  <input id="username" name="username" required="" type="text" />
-  <label for="email">Email:</label>
-  <input id="email" name="email" required="" type="email" />
-  <label for="password">Password:</label>
-  <input id="password" name="password" required="" type="password" />
-  <input name="register" type="submit" value="Register" />
-</form>
+<form id="account" action="#" method="POST">
+        <div id="container1">
+            <div id="container2">
+                <h1 id="title">Create account</h1>
+                <div class="prog-bar"></div>
+                <div id="step-1" class="steps">
+                    <label class="box_label">First Name</label>
+                    <input class="input_box" required>
+                    <label class="box_label">Last Name</label>
+                    <input class="input_box" required>
+                    <label class="box_label">Username</label>
+                    <input class="input_box" required>
+                    <label class="box_label">Password</label>
+                    <input class="input_box" required>
+                </div>
+                <button class="btn" id="cnt" type="button">Continue</button>
+            </div>
+            
+        </div>
+    </form>
+    <script type="text/javascript" src="step.js"></script>
+</body>
+</html>
 
 ?php if (isset($_POST['register'])) {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
