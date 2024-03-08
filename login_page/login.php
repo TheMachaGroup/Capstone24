e8340b998c036b80d0d26a44387a08cc489f6386
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 10:04:38 -0500

    Update login.php

--- login_page/login.php
+++ login_page/login.php
@@ -22,7 +22,7 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) === 1) {
         $row = mysqli_fetch_assoc($result);
         if ($row['Username'] === $Username && $row['UserPassword'] === $Password) {
-            echo "hello";
+            header("Location: https://usarcent.azurewebsites.net/form.html");
             exit();
     } else {
         echo "Invalid credentials";
