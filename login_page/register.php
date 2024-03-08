58ba893f3e4d2df6cc7eca0b6488a56ee6fc9d5f
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-08 12:06:33 -0500

    Update register.php

--- login_page/register.php
+++ login_page/register.php
@@ -1,32 +1,33 @@
-<?php
+h<?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
+echo "connected to database"; 
+    
     // Get the form data
-    $firstName = $_POST['FirstName'];
-    $lastName = $_POST['LastName'];
-    $username = $_POST['Username'];
-    $password = $_POST['UserPassword'];
-    $role = $_POST['Role'];  // Assuming you have an input with name="Role"
-
+    $FirstName = $_POST['FirstName'];
+    $LastName = $_POST['LastName'];
+    $Username = $_POST['Username'];
+    $Password = $_POST['UserPassword'];
+    $Role = $_POST['Role']; 
+echo "received form data";
+    
     // Prepare and bind the SQL statement to check if the username already exists
     $stmt = $conn->prepare('SELECT UserID, UserPassword FROM users WHERE username = ?');
     $stmt->bind_param('s', $username);
     $stmt->execute();
     $stmt->store_result();
-
+    
     // Check if the account already exists in the database
     if ($stmt->num_rows > 0) {
         echo 'Account already exists, please create another!';
     } else {
         // Prepare and bind the SQL statement to insert a new account
         $stmt = $conn->prepare('INSERT INTO users (FirstName, LastName, Username, UserPassword, Role) VALUES (?, ?, ?, ?, ?)');
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-        $stmt->bind_param('sssss', $firstName, $lastName, $username, $hashedPassword, $role);
+    echo "entered into database";
         
         if ($stmt->execute()) {
             Header("Location: https://usarcent.azurewebsites.net/form.html");
