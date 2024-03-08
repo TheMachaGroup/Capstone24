adede35c3aa5026397fd2a61c702c8c97f3f6e48
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-07 23:34:59 -0500

    Create register.php

---
+++ register.php
@@ -0,0 +1,11 @@
+// this code has not been ensure for accuracy 
+
+<form action="register.php" method="post">
+  <label for="username">Username:</label> 
+  <input id="username" name="username" required="" type="text" />
+  <label for="email">Email:</label>
+  <input id="email" name="email" required="" type="email" />
+  <label for="password">Password:</label>
+  <input id="password" name="password" required="" type="password" />
+  <input name="register" type="submit" value="Register" />
+</form>
