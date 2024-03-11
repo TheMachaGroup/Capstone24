9fcc4cb3273e6156eee8040106998f82ea7339d7
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-11 11:25:31 -0400

    Update home_page.php

--- home_page/home_page.php
+++ home_page/home_page.php
@@ -2,7 +2,7 @@
 ob_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
-
+echo "connected to database";
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
