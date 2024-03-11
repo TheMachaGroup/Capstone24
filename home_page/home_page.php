f68f758ad404993c17ed590844af01087774af88
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-11 10:08:32 -0400

    Update home_page.php

--- home_page/home_page.php
+++ home_page/home_page.php
@@ -1,88 +1,44 @@
-<?php 
+<?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-	if ($stmt->fetch()) {
-    // Start output buffering to capture HTML content
-    ob_start();
-    ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../main.css">
-    <link rel="stylesheet" href="account.css">
-    <title>Document</title>
-</head>
-<body>
-	<?php
 
-    // Use a switch statement or if conditions to generate content based on the user role
-    switch ($Role) {
-        case 'ADMIN':
-            echo '<p>Welcome, Admin! You have access to all features.</p>';
-            echo '<nav id="nav-bar">
-		<ul id="nav-bar-list">
-			<li id="id-container"><img src="../img/logo.png" id="logo"><span id="logo_text">HOME</span><img src="../img/icons/three-line.svg" id="collapse" class="collapse-style"></li><br>
-			<li><img src="img/icons/home.svg" class="icon"><a href="home_page/home_page.html">HOME</a></li>
-			<li id="user"><img src="../img/icons/person.svg" class="icon"><span id="user-name">User Name</span></li>
-			<li><img src="img/icons/home.svg" class="icon"><a href="form.html">New Assessment</a></li>
-			<li><img src="img/icons/folder.svg" class="icon"><a href="manage_report/manage_report.html">Report Manager</a></li>
-			<li><img src="img/icons/setting.svg" class="icon"><a href="settings/settings.html">Settings</a></li>
-			<li><img src="img/icons/createaccount.svg" class="icon"><a href="login_page/registerform.php">Create Account</a></li>
-			<li><img src="img/icons/help.svg" class="icon"><a href="help_page/help.html">Help</a></li>
-			<li><img src="img/icons/log-out.svg" class="icon"><a href="index.html">Log Out</a></li>
-		</ul>
-	</nav>';
-            break;
-        case 'ANALYST':
-            echo '<p>Welcome, Analyst! You have limited access.</p>';
-            // Hide the "Create Report" button for Analysts
-	    echo '<nav id="nav-bar">
-		<ul id="nav-bar-list">
-			<li id="id-container"><img src="../img/logo.png" id="logo"><span id="logo_text">HOME</span><img src="../img/icons/three-line.svg" id="collapse" class="collapse-style"></li><br>
-			<li><img src="img/icons/home.svg" class="icon"><a href="home_page/home_page.html">HOME</a></li>
-			<li id="user"><img src="../img/icons/person.svg" class="icon"><span id="user-name">User Name</span></li>
-			<li><img src="img/icons/home.svg" class="icon"><a href="form.html">New Assessment</a></li>
-			<li><img src="img/icons/folder.svg" class="icon"><a href="manage_report/manage_report.html">Report Manager</a></li>
-			<li><img src="img/icons/setting.svg" class="icon"><a href="settings/settings.html">Settings</a></li>
-			<li><img src="img/icons/createaccount.svg" class="icon"><a href="login_page/registerform.php">Create Account</a></li>
-			<li><img src="img/icons/help.svg" class="icon"><a href="help_page/help.html">Help</a></li>
-			<li><img src="img/icons/log-out.svg" class="icon"><a href="index.html">Log Out</a></li>
-		</ul>
-	</nav>';
-            break;
-        // Add more cases for other roles if needed
-        default:
-            echo '<p>Welcome! You have a default role.</p>';
-            break;
+    // Retrieve the user's role from the database based on their login credentials
+    $Username = $_POST['Username'];
+    $Password = $_POST['UserPassword'];
+
+    // Example query to retrieve the user role based on the username and password
+    $query = "SELECT Role FROM users WHERE Username = ? AND UserPassword = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('ss', $Username, $Password);
+    $stmt->execute();
+    $stmt->bind_result($Role);
+
+    // Check if the user exists and get their role
+    if ($stmt->fetch()) {
+        // Redirect the user based on their role
+        switch ($Role) {
+            case 'ADMIN':
+                header("Location: https://usarcent.azurewebsites.net/admin_home.html");
+                exit();
+            case 'ANALYST':
+                header("Location: https://usarcent.azurewebsites.net/analyst_home.html");
+                exit();
+            // Add more cases for other roles if needed
+            default:
+                echo "Welcome! You have a default role.";
+                break;
+        }
+    } else {
+        // User authentication failed, handle accordingly (e.g., redirect to login page)
+        echo "Authentication failed. Please check your credentials.";
     }
 
-    // Your HTML content after the dynamic section
-    ?>
-
-          
-</body>
-</html>
-
-<?php
-
-    // Get the generated HTML content and turn off output buffering
-    $pageContent = ob_get_clean();
-
-    // Output the final HTML content
-    echo $pageContent;
-} else {
-    // User authentication failed, handle accordingly (e.g., redirect to login page)
-    echo "Authentication failed. Please check your credentials.";
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
 }
-
-		
-$stmt->close();
-$conn->close();
 ?>
