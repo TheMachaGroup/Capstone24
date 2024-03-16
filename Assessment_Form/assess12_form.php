826f91792e46af5c87eee9dd2bacfe02e091104e
amruff <70121343+amruff@users.noreply.github.com>
2024-03-16 01:50:33 -0400

    Update assess12_form.php

--- Assessment_Form/assess12_form.php
+++ Assessment_Form/assess12_form.php
@@ -1,1 +1,37 @@
+<?php
+ob_start();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected to database<br>";
+
+    // Retrieve form data
+    $maintain = $_POST['Maintain'];
+    $roofAccess = $_POST['roofAccess'];
+    $groundAccess = $_POST['groundAccess'];
+    $occupants = $_POST['Occupants'];
+    $borc = $_POST['BorC'];
+    $obstruction = $_POST['Obstruction'];
+    $mosques = $_POST['Mosques'];
+    $groundOpenings = $_POST['groundOpenings'];
+    $pointEntry = $_POST['pointEntry'];
+    $BNComments = $_POST['BNComments'];
+
+    // Insert data into standoff_information table
+    $sqlStandoff = "INSERT INTO standoff_information (nObstructions, sObstructions, eObstructions, wObstructions, BNComments) VALUES ('$maintain', '$roofAccess', '$groundAccess', '$occupants', '$borc', '$obstruction', '$mosques', '$groundOpenings', '$pointEntry', '$BNComments')";
+
+    if ($conn->query($sqlStandoff) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error inserting record into standoff_information table: " . $conn->error;
+    }
+
+    // Close the connection
+    $conn->close();
+}
+ob_end_flush();
+?>
 
