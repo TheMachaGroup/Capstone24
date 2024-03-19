797764b5df2e17e75fa19c7cb4171408c0b5a01a
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-19 12:09:17 -0400

    Update assess3_form.php

--- Assessment_Form/assess3_form.php
+++ Assessment_Form/assess3_form.php
@@ -1,23 +1,23 @@
 <?php
 ob_start();
+
+// Database connection
+$conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected to database<br>";
-
-    // Retrieve form data
+    // Retrieve form data from POST request
     $totalRooms = $_POST['totalRooms'];
     $totalPeople = $_POST['totalPeople'];
     $occupancyComments = $_POST['occupancyComments'];
     $BNComments = $_POST['BNComments'];
-    
+
     // Prepare and execute SQL statement to insert data into the database
-    $sql = "INSERT INTO occupancyinformation (totalRooms, totalPeople, occupancyComments, BNComments) VALUES ('$totalRooms', '$totalPeople', '$occupancyComments', '$BNComments')";
-    
-    // Check if the statement was executed successfully
+    $sql = "INSERT INTO occupancyinformation (totalRooms, totalPeople, occupancyComments, Comments)
+            VALUES ('$totalRooms', '$totalPeople', '$occupancyComments', '$BNComments')";
     if ($conn->query($sql) === TRUE) {
         // Redirect to confirmation page
         header("Location: confirmation.html");
@@ -26,8 +26,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // If execution fails, handle the error (e.g., display an error message)
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-    // Close the connection
-    $conn->close();
 }
+
+// Close the connection
+$conn->close();
+
 ob_end_flush();
 ?>
