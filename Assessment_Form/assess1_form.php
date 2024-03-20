b679bb13e8853f120809b520a701881497085735
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-20 19:24:57 -0400

    Update assess1_form.php

--- Assessment_Form/assess1_form.php
+++ Assessment_Form/assess1_form.php
@@ -1,12 +1,27 @@
+//The code below connects the login page to the database to validate the credentials in order to log in
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+echo "testing";
+try {
+    $conn = mysqli_init();
+    mysqli_real_connect($conn, "usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+// Example query
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+// Check if the query was successful
+if ($result === false) {
+    // Handle the error
+    echo "Error executing query: " . $conn->error;
+} else {
+    // Fetch and display the data
+    while ($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["UserID"] . " - Name: " . $row["Username"] . "<br>";
     }
-echo "Connected to database";
-
-
-
-?>
+}
+    
+  ?> 
