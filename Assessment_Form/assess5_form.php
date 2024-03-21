e903b4c09d8d7e9702d46cdd9f6c2b3b7a3f6410
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-21 13:51:24 -0400

    Update assess5_form.php

--- Assessment_Form/assess5_form.php
+++ Assessment_Form/assess5_form.php
@@ -1,31 +1,25 @@
 <?php
-ob_start();
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+// Establish a connection to the MySQL database
+$conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected to database<br>";
-
-    // Retrieve form data
-    $parkingArea = $_POST['ParkingArea'];
-    $parkSec = $_POST['ParkSec'];
-    $light = $_POST['Light'];
-    $pComments = $_POST['pComments'];
-    $bnComments = $_POST['BNComments'];
-
-    // Perform SQL insertion
-    $sql = "INSERT INTO parkinginformation (LocationOfParking, SecurityForParking, Lighting, Comments)
-            VALUES ('$parkingArea', '$parkSec', '$light', '$pComments')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Record inserted successfully";
-    } else {
-        echo "Error inserting into table: " . $conn->error;
-    }
-  
-    // Close the connection
-    $conn->close();
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-ob_end_flush();
+
+// Retrieve form data
+$parkingArea = $_POST['ParkingArea'];
+$parkSec = $_POST['ParkSec'];
+$light = $_POST['Light'];
+$pComments = $_POST['pComments'];
+
+// Perform SQL insertion
+$sql = "INSERT INTO parkinginformation (LocationOfParking, SecurityForParking, Lighting, Comments)
+        VALUES ('$parkingArea', '$parkSec', '$light', '$pComments')";
+if ($conn->query($sql) === TRUE) {
+    echo "inserted into parking information table";
+}
+  
+// Close the database connection when done
+mysqli_close($conn);
 ?>
