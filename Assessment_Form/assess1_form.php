95f52d017ee0c379f47fdbf41b1dfb457b07ffc0
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-20 20:54:03 -0400

    Update assess1_form.php

--- Assessment_Form/assess1_form.php
+++ Assessment_Form/assess1_form.php
@@ -1,12 +1,11 @@
 <?php
-
+ob_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
     echo "Connected to database<br>";
 
     // Retrieve form data
