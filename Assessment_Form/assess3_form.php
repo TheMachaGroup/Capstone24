ad2ea9330acefe68d6caab2dd859c60a6ce042c1
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-21 11:31:45 -0400

    Update assess3_form.php

--- Assessment_Form/assess3_form.php
+++ Assessment_Form/assess3_form.php
@@ -1,35 +1,27 @@
 <?php
-ob_start();
+// Establish a connection to the MySQL database
+$conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
-// Database connection
-$conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Retrieve form data from POST request
+    $numBuildings = $_POST['buildings'];
+    $numFloors = $_POST['floors'];
     $totalRooms = $_POST['totalRooms'];
     $totalPeople = $_POST['totalPeople'];
     $occupancyComments = $_POST['occupancyComments'];
     $BNComments = $_POST['BNComments'];
 
     // Prepare and execute SQL statement to insert data into the database
-    $sql = "INSERT INTO occupancyinformation (totalRooms, totalPeople, occupancyComments, Comments)
-            VALUES ('$totalRooms', '$totalPeople', '$occupancyComments', '$BNComments')";
+    $sql = "INSERT INTO occupancyinformation (TotalBuildings, NumFloors, TotalAptInComp, TotalOccByPersonnel, Comments, BNComments)
+            VALUES ('buildings', 'floors', '$totalRooms', '$totalPeople', '$occupancyComments', '$BNComments')";
     if ($conn->query($sql) === TRUE) {
-        // Redirect to confirmation page
-        header("Location: confirmation.html");
-        exit();
-    } else {
-        // If execution fails, handle the error (e.g., display an error message)
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+        echo "Successfully inserted into Occupancy information";
 }
-
-// Close the connection
-$conn->close();
-
-ob_end_flush();
+// Close the database connection when done
+mysqli_close($conn);
 ?>
