0c44bf6d543fcf07b2abd980711ffc526bf9a02d
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-20 21:31:32 -0400

    Update assess1_form.php

--- Assessment_Form/assess1_form.php
+++ Assessment_Form/assess1_form.php
@@ -6,34 +6,43 @@ $conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide",
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-    // Retrieve form data
-    $reportName = $_POST['HousingAssessment'];
-    $reportdate = $_POST['reportdate'];
-    $buildingName = $_POST['fname'];
-    $gpsLocation = $_POST['gps'];
 
-    // Insert data into locationdetails table
-    $sqlLocation = "INSERT INTO locationdetails (LocationName) VALUES ('$reportName')"; 
-echo "data inserted in location details"; 
+// Retrieve form data
+$reportName = $_POST['HousingAssessment'];
+$reportdate = $_POST['reportdate'];
+$buildingName = $_POST['fname'];
+$gpsLocation = $_POST['gps'];
 
+// Insert data into locationdetails table
+$sqlLocation = "INSERT INTO locationdetails (LocationName) VALUES ('$reportName')";
 if ($conn->query($sqlLocation) === TRUE) {
-        // Retrieve the ID of the last inserted record
-        $locationId = $conn->insert_id;
-echo "id retrieve from location details"; 
-    
-        // Insert data into GeographicLocation table
-        $sqlGeo = "INSERT INTO GeographicLocation (GPSLocation, LocationID) VALUES ('$gpsLocation', '$locationId')";
-echo "data inserted into geographic location"; 
-    
-        if ($conn->query($sqlGeo) === TRUE) {
-            // Retrieve the ID of the last inserted record
-            $geoLocationId = $conn->insert_id;
-        echo "id retrieved"; 
+    echo "Data inserted in location details<br>";
+} else {
+    echo "Error inserting record into locationdetails table: " . $conn->error . "<br>";
+}
 
-            // Insert data into Form table with reference to GeographicLocation and locationdetails tables
-            $sqlForm = "INSERT INTO Form (ReportName, BuildingName, GeoLocationID, LocationID, DateOfReport) VALUES ('$reportName', '$buildingName', '$geoLocationId', '$locationId', '$reportdate')";
-    echo "data inserted into form table"; 
-            
-// Close the database connection when done - testing to see if this works
+// Retrieve the ID of the last inserted record
+$locationId = $conn->insert_id;
+
+// Insert data into GeographicLocation table
+$sqlGeo = "INSERT INTO GeographicLocation (GPSLocation, LocationID) VALUES ('$gpsLocation', '$locationId')";
+if ($conn->query($sqlGeo) === TRUE) {
+    echo "Data inserted into GeographicLocation<br>";
+} else {
+    echo "Error inserting record into GeographicLocation table: " . $conn->error . "<br>";
+}
+
+// Retrieve the ID of the last inserted record
+$geoLocationId = $conn->insert_id;
+
+// Insert data into Form table with reference to GeographicLocation and locationdetails tables
+$sqlForm = "INSERT INTO Form (ReportName, BuildingName, GeoLocationID, LocationID, DateOfReport) VALUES ('$reportName', '$buildingName', '$geoLocationId', '$locationId', '$reportdate')";
+if ($conn->query($sqlForm) === TRUE) {
+    echo "Data inserted into Form table<br>";
+} else {
+    echo "Error inserting record into Form table: " . $conn->error . "<br>";
+}
+
+// Close the database connection when done
 mysqli_close($conn);
 ?>
