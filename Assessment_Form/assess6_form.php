cc600d80b6d0a4e4a9acc2230dcebeee6f8071ea
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-21 14:08:49 -0400

    Update assess6_form.php

--- Assessment_Form/assess6_form.php
+++ Assessment_Form/assess6_form.php
@@ -1,39 +1,29 @@
 <?php
-ob_start();
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+// Establish a connection to the MySQL database
+$conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected to database<br>";
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-    // Retrieve form data
-    $monitored_outside = $_POST["monitored_outside"];
-    $entry_controlled = $_POST["entry_controlled"];
-    $comments = $_POST["BNComments"];
+// Retrieve form data
+$monitored_outside = $_POST["monitored_outside"];
+$entry_controlled = $_POST["entry_controlled"];
+$comments = $_POST["BNComments6"];
 
-    // Insert data into the database
-    $sql = "INSERT INTO entryandcirculationtable (vehiclesmonitored, entrycontrolled) VALUES ('$monitored_outside', '$entry_controlled')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Record inserted successfully<br>";
-    } else {
-        echo "Error inserting into entryandcirculationtable: " . $conn->error;
-    }
-
-    // Get the ID of the last inserted row
-    $entryandcircID = $conn->insert_id;
-
-    // Insert data into the form table
-    $sql = "INSERT INTO form_table (entryandcircID, comments) VALUES ('$entryandcircID', '$comments')";
-    if ($conn->query($sql) === TRUE) {
-        echo "Record inserted successfully<br>";
-    } else {
-        echo "Error inserting into form_table: " . $conn->error;
-    }
+// Insert data into the database
+$sql = "INSERT INTO entryandcirculationinformation (VehiclesMonitored, EntryControlled, Comments) 
+VALUES ('$monitored_outside', '$entry_controlled', '$comments')";
+if ($conn->query($sql) === TRUE) {
+    echo "inserted successfully into table<br>";
+}
 
     // Close the connection
     $conn->close();
 }
-ob_end_flush();
+
+
+// Close the database connection when done
+mysqli_close($conn);
 ?>
