affecb01138a81a416a6e6204ede622690ae24c7
amruff <70121343+amruff@users.noreply.github.com>
2024-03-21 22:18:54 -0400

    Update assess13_form.php

--- Assessment_Form/assess13_form.php
+++ Assessment_Form/assess13_form.php
@@ -1,13 +1,11 @@
 <?php
-ob_start();
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+// Establish a connection to the MySQL database
+$conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected to database<br>";
-
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
    // Retrieve form data for police
     $policeStation = $_POST['policeStation'];
     $policeDistance = $_POST['policeDistance'];
@@ -39,10 +37,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "Error: " . $sqlFire . "<br>" . $conn->error;
     }
 
-    // Close the statement and connection
-    $stmtContingency->close();
-    $conn->close();
-}
-ob_end_flush();
+   // Close the database connection when done
+mysqli_close($conn);
 ?>
-
