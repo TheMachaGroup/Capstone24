68909488e360ff02b134635da9cc7b3980e92a10
TheMachaGroup <160083105+TheMachaGroup@users.noreply.github.com>
2024-03-22 18:49:12 -0400

    Update assess1_form.php

--- Assessment_Form/assess1_form.php
+++ Assessment_Form/assess1_form.php
@@ -16,20 +16,13 @@ $spaceType = $_POST['spaceType'];
 
 // Insert data into locationdetails table
 $sqlLocation = "INSERT INTO locationdetails (LocationName, PhoneNumber, TypeofResidence, TypeofUnit) VALUES ('$reportName', '$phoneNumber', '$unitType', '$spaceType')";
-if ($conn->query($sqlLocation) === TRUE) {
-    echo "Data inserted in location details<br>";
-} else {
-    echo "Error inserting record into locationdetails table: " . $conn->error . "<br>";
-}
 
 
 // Insert data into Form table with reference to GeographicLocation and locationdetails tables
 $sqlForm = "INSERT INTO form (DateofReport) VALUES ('$reportdate')";
 if ($conn->query($sqlForm) === TRUE) {
-    echo "Data inserted into Form table<br>";
-} else {
-    echo "Error inserting record into Form table: " . $conn->error . "<br>";
-}
+    header("Location: https://usarcent.azurewebsites.net/Form.html");
+                    exit();
 
 // Close the database connection when done
 mysqli_close($conn);
