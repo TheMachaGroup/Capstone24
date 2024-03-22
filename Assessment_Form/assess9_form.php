756868bc6a274f7e5622bde252705b82e21251cd
amruff <70121343+amruff@users.noreply.github.com>
2024-03-21 23:05:40 -0400

    Update assess9_form.php

--- Assessment_Form/assess9_form.php
+++ Assessment_Form/assess9_form.php
@@ -6,7 +6,6 @@ $conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide",
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
     // Retrieve form data
     $complexDistance = $_POST['ComplexDistance'];
     $reinforced = $_POST['Reinforced'];
@@ -14,20 +13,18 @@ if (!$conn) {
     $reStairwell = $_POST['ReStairwell'];
     $BNcomments = $_POST['BNComments9'];
 
-
     // SQL query to insert data into rallypointsinfo table
     $sql_RallyPointsInfo = "INSERT INTO rallypointsinfo (ParkingDistance, ReinforcedConcBasementOrParking, ShieldedEvacSiteMeters, ReinforcedConcStairwell) VALUES ('$complexDistance', '$reinforced', '$shield', '$reStairwell')";
-if ($conn->query($sql_RallyPointsInfo) === TRUE) {
-    echo "Data inserted in location details<br>";
+    if ($conn->query($sql_RallyPointsInfo) === TRUE) {
+        echo "Data inserted in location details<br>";
 } 
 
  // Insert data into Form table with reference to rallypointsinfo table
-            $sql_form = "INSERT INTO form (DateofReport, RallyPointsID) VALUES ('$reportdate', '$rallyPointsID')";
-            if ($conn->query($sql_form) === TRUE) {
-                echo "Data inserted into Form table<br>";
-            } 
+     $sqlForm = "INSERT INTO form (DateofReport, RallyPointsID) VALUES ('$reportdate', '$rallyPointsID')";
+         if ($conn->query($sqlForm) === TRUE) {
+             echo "Data inserted into Form table<br>";
+     } 
 
       // Close connection
     mysqli_close($conn);
-
 ?>
