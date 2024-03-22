976d6a57c5f243f4160f6869129a6c774b5fc82f
amruff <70121343+amruff@users.noreply.github.com>
2024-03-21 22:07:49 -0400

    Update assess10_form.php

--- Assessment_Form/assess10_form.php
+++ Assessment_Form/assess10_form.php
@@ -1,12 +1,11 @@
 <?php
-ob_start();
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $conn = new mysqli("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
+// Establish a connection to the MySQL database
+$conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide", "0LB5E265UCUE1D5E$", "usarcent-database", 3306);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "Connected to database<br>";
+// Check if the connection was successful
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
    / // Retrieve form data
     $perimeterBarriers = $_POST['PB'];
@@ -23,9 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-    //Close connection
-      $stmt->close();
-    $conn->close();
-}
-ob_end_flush();
+
+   // Close the database connection when done
+mysqli_close($conn);
 ?>
