ef9e1804ade9d8366224a281ed4f78d10017fc4a
amruff <70121343+amruff@users.noreply.github.com>
2024-03-24 23:19:58 -0400

    Update assess9_form.php

--- Assessment_Form/assess9_form.php
+++ Assessment_Form/assess9_form.php
@@ -9,14 +9,14 @@ if (!$conn) {
 
 // Retrieve form data
 $parkingDistance = $_POST['ParkingDistance'];
-$reinforcedoncBasementOrParking = $_POST['ReinforcedConcBasementOrParking'];
+$reinforcedConcBasementOrParking = $_POST['Reinforced'];
 $shieldedEvacSiteMeters = $_POST['ShieldedEvacSiteMeters'];
 $reinforcedConcStairwell = $_POST['ReinforcedConcStairwell'];
 $BNcomments = $_POST['BNComments9'];
 
 // SQL query to insert data into rallypointsinfo table
 $sql = "INSERT INTO rallypointsinfo (ParkingDistance, ReinforcedConcBasementOrParking, ShieldedEvacSiteMeters, ReinforcedConcStairwell, BNComments) 
-VALUES ('$parkingDistance', '$reinforcedConcBasementaOrParking', '$shieldedEvacSiteMeters', '$reinforcedConcStairwell', '$BNcomments')";
+VALUES ('$parkingDistance', '$reinforcedConcBasementOrParking', '$shieldedEvacSiteMeters', '$reinforcedConcStairwell', '$BNcomments')";
 
 // Execute the query
 if ($conn->query($sql) === TRUE) {
