ba07653e25f896640dafe6bc41671d79d977de68
amruff <70121343+amruff@users.noreply.github.com>
2024-03-27 19:35:28 -0400

    Update assess13_form.php

--- Assessment_Form/assess13_form.php
+++ Assessment_Form/assess13_form.php
@@ -13,7 +13,7 @@ $policeDistance = $_POST['policeDistance'];
 $policeNumber = $_POST['policeNumber'];
 $policeTime = $_POST['policeTime'];
 
-$policeSql = "INSERT INTO policeforce (PoliceForce, PoliceDistance, PoliceNumber, PoliceTime) 
+$sqlPolice = "INSERT INTO policeforce (PoliceForce, PoliceDistance, PoliceNumber, PoliceTime) 
 VALUES ('$policeForce', '$policeDistance', '$policeNumber', '$policeTime')";
 
 // Retrieve form data for fire
@@ -22,7 +22,7 @@ $fireDistance = $_POST['fireDistance'];
 $fireNumber = $_POST['fireNumber'];
 $fireTime = $_POST['fireTime'];
 
-$fireSql = "INSERT INTO firedepartment (FireDepartment, FireDistance, FireNumber, FireTime) 
+$sqlFire = "INSERT INTO firedepartment (FireDepartment, FireDistance, FireNumber, FireTime) 
 VALUES ('$fireDepartment', '$fireDistance', '$fireNumber', '$fireTime')";
 
 //Retrieve form data for hospital
@@ -31,17 +31,17 @@ $distance = $_POST['distance'];
 $hospitalNumber = $_POST['hospitalNumber'];
 $hospitalResponseTime = $_POST['hospitalResponseTime'];
 
-$hospitalSql = "INSERT INTO hospital (HospitalLocation, Distance, HospitalNumber, HospitalResponseTime) 
+$sqlHospital = "INSERT INTO hospital (HospitalLocation, Distance, HospitalNumber, HospitalResponseTime) 
 VALUES ('$hospitalLocation', '$distance', '$hospitalNumber', '$hospitalResponseTime')";
 
 // Execute SQL queries
-if ($conn->query($policeSql) === TRUE && $conn->query($fireSql) === TRUE && $conn->query($hospitalSql) === TRUE) {
+if ($conn->query($sqlPolice) === TRUE && $conn->query($sqlFire) === TRUE && $conn->query($sqlHospital) === TRUE) {
     header("Location: https://usarcent.azurewebsites.net/Form.html");
     exit();
 } else {
     echo "Error: " . $conn->error;
 }
 
-   // Close the database connection when done
+// Close the database connection when done
 mysqli_close($conn);
 ?>
