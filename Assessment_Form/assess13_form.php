3eea83ecfd6a426a9bbbf99ed26c8ed93ccb31c9
amruff <70121343+amruff@users.noreply.github.com>
2024-03-26 22:20:52 -0400

    Update assess13_form.php

--- Assessment_Form/assess13_form.php
+++ Assessment_Form/assess13_form.php
@@ -6,52 +6,41 @@ $conn = mysqli_connect("usarcent-server.mysql.database.azure.com", "thpgbqeide",
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-   // Retrieve form data for police
-    $policeStation = $_POST['policeStation'];
-    $policeDistance = $_POST['policeDistance'];
-    $policeNumber = $_POST['policeNumber'];
-    $policeTime = $_POST['policeTime'];
 
-    // Retrieve form data for fire
-    $fireStation = $_POST['fireStation'];
-    $fireDistance = $_POST['fireDistance'];
-    $fireNumber = $_POST['fireNumber'];
-    $fireTime = $_POST['fireTime'];
+// Retrieve form data for police
+$policeStation = $_POST['policeStation'];
+$policeDistance = $_POST['policeDistance'];
+$policeNumber = $_POST['policeNumber'];
+$policeTime = $_POST['policeTime'];
 
-    //Retrieve form data for hospital
-    $hospitalLocation = $_POST['hospitalLocation'];
-    $Distance = $_POST['distance'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $hospitalResponseTime = $_POST['hospitalTime'];
+// Retrieve form data for fire
+$fireDepartment = $_POST['fireDepartment'];
+$fireDistance = $_POST['fireDistance'];
+$fireNumber = $_POST['fireNumber'];
+$fireTime = $_POST['fireTime'];
+
+//Retrieve form data for hospital
+$hospital = $_POST['hospital'];
+$Distance = $_POST['distance'];
+$phoneNumber = $_POST['phoneNumber'];
+$hospitalResponseTime = $_POST['hospitalTime'];
 
    
-    // SQL query to insert data into policeforce table
-    $sql_Police = "INSERT INTO policeforce (PoliceStation, PoliceDistance, PhoneNumber, PoliceTime) VALUES ('$policeStation', '$policeDistance', '$phoneNumber', '$policeTime')";
+// SQL query to insert data into policeforce table
+$sql = "INSERT INTO policeforce (PoliceStation, PoliceDistance, PoliceNumber, PoliceTime) 
+VALUES ('$policeStation', '$policeDistance', '$phoneNumber', '$policeTime')";
 
-    if ($conn->query($sql_Police) === TRUE) {
-        echo "New police record created successfully";
-    }
+// SQL query to insert data into firedepartment table
+$sql = "INSERT INTO firedepartment (FireDepartment, FireDistance FireNumber, FireTime) 
+VALUES ('$fireDepartment', '$fireDistance', '$phoneNumber', '$fireTime')";
 
-    // SQL query to insert data into firedepartment table
-    $sql_Fire = "INSERT INTO firedepartment (FireStation, FireDistance, PhoneNumber, FireTime) VALUES ('$fireStation', '$fireDistance', '$phoneNumber', '$fireTime')";
+$sql = "INSERT INTO hospital (Hospital, Distance, HospitalNumber, HospitalResponseTime) 
+VALUES ('$hospital', '$Distance', '$phoneNumber', '$hospitalResponseTime')";
 
-    if ($conn->query($sql_Fire) === TRUE) {
-        echo "New fire record created successfully";
-    } 
-    if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) === TRUE) {
     header("Location: https://usarcent.azurewebsites.net/Form.html");
     exit();
-    }
-
-    $sql = "INSERT INTO hospital (HospitalLocation, Distance, PhoneNumber, HospitalResponseTime) 
-        VALUES ('$hospitalLocation', '$Distance', '$phoneNumber', '$hospitalResponseTime')";
-     if ($conn->query($sql_Fire) === TRUE) {
-        echo "New fire record created successfully";
-    } 
-    if ($conn->query($sql) === TRUE) {
-    header("Location: https://usarcent.azurewebsites.net/Form.html");
-    exit();
-    }
+}
 
 
    // Close the database connection when done
