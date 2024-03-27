0bf6916b53a6b4976cb4698b92c0f31bba9a9af6
amruff <70121343+amruff@users.noreply.github.com>
2024-03-26 23:07:29 -0400

    Update assess15_form.php

--- Assessment_Form/assess15_form.php
+++ Assessment_Form/assess15_form.php
@@ -7,18 +7,19 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
- // Retrieve form data
-    $rsoComments = $_POST['RSOComments'];
-    $miscellaneousInfo = $_POST['miscellaneousInfo'];
-    $BNComments = $_POST['BNComments15'];
 
-    // Prepare SQL statement to insert data into annualassessment table
-    $sql = "INSERT INTO annualassessment (RSOComments, MiscellaneousInfo, BNComments) VALUES ('$rsoComments', '$miscellaneousInfo', '$BNComments')";
-         if ($conn->query($sql) === TRUE) {
+// Retrieve form data
+$rsoComments = $_POST['RSOComments'];
+$miscellaneousInfo = $_POST['miscellaneousInfo'];
+
+// Prepare SQL statement to insert data into annualassessment table
+$sql = "INSERT INTO annualassessment (RSOComments, MiscellaneousInfo) 
+VALUES ('$rsoComments', '$miscellaneousInfo')";
+if ($conn->query($sql) === TRUE) {
     header("Location: https://usarcent.azurewebsites.net/Form.html");
     exit();
-    } 
+} 
 
-  // Close the database connection when done
+// Close the database connection when done
 mysqli_close($conn);
 ?>
